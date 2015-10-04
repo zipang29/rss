5b4265461de25e6ac0c0c5181f359cde5af9e7eb
Nailja <nailjabogrova@gmail.com>
2015-10-03 22:04:44 -0200

    new class Item

--- parser/parser/Item.cpp
+++ parser/parser/Item.cpp
@@ -2,6 +2,71 @@
 
 Item::Item()
 {
-
+	
 }
+
+Item::~Item()
+{
+	
+}
+
+QString Item::get_id(){
+  return id;
+} 
+
+void Item::set_id(Qstring i){
+  id = i;
+}
+
+QString Item::get_url_du_flux(){
+  return url_du_flux;
+} 
+
+void Item::set_url_du_flux(Qstring udf){
+  url_du_flux = udf;
+}
+QString Item::get_url_de_la_page(){
+  return url_de_la_page;
+} 
+
+void Item::set_url_de_la_page(Qstring udp){
+  url_de_la_page = udp;
+}
+QString Item::get_titre(){
+  return titre;
+} 
+
+void Item::set_titre(Qstring t){
+  titre = t;
+}
+QString Item::get_description(){
+  return description;
+} 
+
+void Item::set_description(Qstring des){
+  description = des;
+}
+QString Item::get_resume(){
+  return resume;
+} 
+
+void Item::set_resume(Qstring r){
+  resume = r;
+}
+QString Item::get_langue(){
+  return langue;
+} 
+
+void Item::set_langue(Qstring l){
+  langue = l;
+}
+
+QDateTime Item::get_date(){
+  return date;
+} 
+
+void Item::set_date(QDateTime d){
+  date = d;
+}
+
 
--- parser/parser/Item.h
+++ parser/parser/Item.h
@@ -1,11 +1,35 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <QString>
+#include <QDateTime>
 
 class Item
 {
 public:
     Item();
-};
+	~Item();
+	QString get_id();
+	QString get_url_du_flux();
+	QString get_url_de_la_page();
+	QString get_titre();
+	QString get_description();
+	QString get_resume();
+	QString get_langue();
+	QDateTime get_date();
+	
+	void set_id(Qstring i);
+	void set_url_du_flux(Qstring udf);
+	void set_url_de_la_page(Qstring udp);
+	void set_titre(Qstring t);
+	void set_description(Qstring des);
+	void set_resume(Qstring r);
+	void set_langue(Qstring l);
+	void set_date(QDateTime d);
 
+private:
+    QString url_du_flux, url_de_la_page, titre, description, resume, langue;
+    QDateTime date;
+    QString id;
+};
 #endif // ITEM_H
