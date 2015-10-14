a2860c2fb30de2a4181412d0acc5fe212bdded3b
Guillaume Claudic <guillaume.claudic@gmail.com>
2015-10-14 16:19:35 +0200

    Ajout du fichier des constantes

---
+++ parser/parser/constantes.h
@@ -0,0 +1,39 @@
+#ifndef CONSTANTES
+#define CONSTANTES
+
+
+/**
+  * Définition des constantes pour le parsing
+  */
+#define RSS "rss"
+
+#define CHANNEL "channel"
+
+#define LAST_BUILD_DATE "lastBuildDate"
+
+#define ITEM "item"
+
+#define TITLE "title"
+
+#define CATEGORY "category"
+
+#define LINK "link"
+
+#define DESCRIPTION "description"
+
+#define THUMBNAIL "thumbnail"
+
+#define ENCLOSURE "enclosure"
+
+#define GUID "guid"
+
+#define PUB_DATE "pubDate"
+
+#define SOURCE "source"
+
+#define AUTHOR "author"
+
+#define LANGUAGE "language"
+
+#endif // CONSTANTES
+
