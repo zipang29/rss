122dd6868d79b089b0f84c73ab0e88fcdefca965
Guillaume Claudic <guillaume.claudic@gmail.com>
2015-11-23 22:00:52 +0100

    Correction des problèmes de crash du programme. Le problème des const TCHAR* est résolu dans l'indexeur. Il faut penser à supprimer à chaque fois que l'on arrête brutalement le programme le dossier bdd.db sinon le programme plantera à la prochaine exécution car les fichiers seront toujours 'lock'

--- moteur de recherche/src_parser/Indexeur.cpp
+++ moteur de recherche/src_parser/Indexeur.cpp
@@ -15,28 +15,25 @@ Indexeur::Indexeur(QString dbPath)
 
 void Indexeur::indexing(Item * item)
 {
-    /*Document * doc = new Document();
-	Field * f = new Field("url_du_flux", item->get_url_du_flux().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
+    Document * doc = new Document();
+	Field * f = new Field("url_du_flux", (const TCHAR*)item->get_url_du_flux().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
     doc->add(*f);
-    f = new Field("url_de_la_page", item->get_url_de_la_page().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
+	f = new Field("url_de_la_page", (const TCHAR*)item->get_url_de_la_page().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
     doc->add(*f);
-    f = new Field("titre", item->get_titre().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
+	f = new Field("titre", (const TCHAR*)item->get_titre().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
     doc->add(*f);
-    f = new Field("description", item->get_description().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
+	f = new Field("description", (const TCHAR*)item->get_description().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
     doc->add(*f);
-    f = new Field("contenu", item->get_contenu().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
+	f = new Field("contenu", (const TCHAR*)item->get_contenu().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
     doc->add(*f);
-    f = new Field("langue", item->get_langue().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
+	f = new Field("langue", (const TCHAR*)item->get_langue().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
     doc->add(*f);
-    f = new Field("category", item->get_category().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
+	f = new Field("category", (const TCHAR*)item->get_category().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
     doc->add(*f);
-    f = new Field("date", item->get_date().toString().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
+	f = new Field("date", (const TCHAR*)item->get_date().toString().toStdWString().c_str(), Field::STORE_YES, Field::INDEX_TOKENIZED);
     doc->add(*f);
 
     writer->addDocument(doc, writer->getAnalyzer());
-	*/
-    
-
 }
 
 Indexeur::~Indexeur()
